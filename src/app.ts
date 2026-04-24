cb9cc9059bcdf34298e67c768789020a9b7d1a82
Dmytro Shvets <d.shvets.business@gmail.com>
2026-04-24 21:11:06 +0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import helmet from 'helmet';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(helmet());
+
+app.use(cors({ origin: '*' }));
 
 app.use(routes);
 
